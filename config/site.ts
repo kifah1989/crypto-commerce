d908b5a6d19caa3debf47b2fd97210891a11987f
kifah <kifah.andary@gmail.com>
2024-11-11 21:48:08 +0200

    updated something

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-    name: 'Next.js + NextUI',
-    description:
-        'Make beautiful websites regardless of your design experience.',
+    name: 'Siraj is Gay',
+    description: 'Byklo fr',
     navItems: [
         {
             label: 'Home',

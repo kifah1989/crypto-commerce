27057e227e6e0c74740fc4235c02ffc0715409e9
kifah <kifah.andary@gmail.com>
2024-11-23 15:58:35 +0200

    auth middleware

--- config/auth.ts
+++ config/auth.ts
@@ -13,7 +13,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         Resend({
             // If your environment variable is named differently than default
             apiKey: process.env.AUTH_RESEND_KEY,
-            from: 'Acme <onboarding@resend.dev>',
+            from: 'Crypto Commerce <onboarding@resend.dev>',
         }),
     ],
+    pages: {
+        signIn: '/signin',
+    },
 })

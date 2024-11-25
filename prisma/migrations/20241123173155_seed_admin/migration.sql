1140ed7cdbb597db1cedeed215ec8347dc859e95
kifah <kifah.andary@gmail.com>
2024-11-25 16:39:18 +0200

    removed migration history

--- prisma/migrations/20241123173155_seed_admin/migration.sql
+++
@@ -1,3 +0,0 @@
--- seed admin role for email kifah.andary@gmail.com role = admin
-INSERT INTO "Users" ("id", "email", "role", "createdAt", "updatedAt") 
-VALUES ('cm3ujenno000008la9mnmfqgw', 'kifah.andary@gmail.com', 'admin', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
